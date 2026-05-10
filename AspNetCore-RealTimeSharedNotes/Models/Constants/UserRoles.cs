5caa3beda17bd02b80d9a412a1963ae27647623c
97131004 <56445802+97131004@users.noreply.github.com>
2026-05-11 01:09:31 +0200

    Update UserRoles.cs

--- AspNetCore-RealTimeSharedNotes/Models/Constants/UserRoles.cs
+++ AspNetCore-RealTimeSharedNotes/Models/Constants/UserRoles.cs
@@ -1,5 +1,6 @@
 namespace AspNetCore_RealTimeSharedNotes.Models.Constants;
 
+//important: changes require a database rebuild from scratch
 public static class UserRoles
 {
     public const string SuperAdmin = "superadmin";
